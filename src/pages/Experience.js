7196aca4b87843422f8e90e2434917a09b2d8b48
Hristijan <hristijan_risteski@live.com>
2023-03-30 12:05:23 +0200

    Removed useState import

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import "../styles/Experience.css";
 import certificate1 from "../assets/certificate1.jpg";
 import certificate2 from "../assets/certificate2.jpg";
